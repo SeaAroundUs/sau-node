6b65bafe18c480bab86dbb59e1b2cdcd0400a73c
Robert Reis <T-RobertR@vulcan.com>
2015-09-30 13:20:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+global.__base = __dirname + '/';
+
+var q = require('q');
+var util = require(__base + 'lib/util');
+var Region = require(__base + 'lib/region');
+
+module.exports = {
+  getRegions: function(regionName) {
+    var url = regionName + '/?nospatial=true';
+    var deferred = q.defer();
+
+    util.callAPI(url).then(function(regions) {
+      deferred.resolve(regions.map(function(region) {
+        region.region = regionName;
+        return new Region(region);
+      }));
+    });
+
+    return deferred.promise;
+  },
+
+  getRegionDetail: function(regionName, regionId) {
+    var region = new Region({ region: regionName, id: regionId});
+    return region.getDetail();
+  },
+
+  getRegionData: function(regionName, regionId, params) {
+    var region = new Region({ region: regionName, id: regionId});
+    return region.getData(params);
+  }
+};
